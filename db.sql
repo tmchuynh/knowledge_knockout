290e2bb415b5c1dd68aa42a4992236fd37da1f73
Tina Huynh <tinamchuynh@gmail.com>
2024-11-08 19:11:42 -0800

    fix config

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
 -- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS quizlet;
+CREATE DATABASE IF NOT EXISTS knowledge_knockout_db;
 
 -- Use the newly created database
-USE quizlet;
+USE knowledge_knockout_db;
 
 -- Create users table
 CREATE TABLE IF NOT EXISTS users (
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS questions (
         'true_false',
         'written'
     ) NOT NULL DEFAULT 'multiple_choice',
-    FOREIGN KEY (quiz_id, level) REFERENCES quizzes (quiz_id, level) ON DELETE CASCADE
+    FOREIGN KEY (quiz_id) REFERENCES quizzes (quiz_id) ON DELETE CASCADE
 );
 
 -- Create answers table
@@ -46,33 +46,6 @@ CREATE TABLE IF NOT EXISTS answers (
     FOREIGN KEY (question_id) REFERENCES questions (question_id) ON DELETE CASCADE
 );
 
--- Create progress table
-CREATE TABLE IF NOT EXISTS progress (
-    progress_id VARCHAR(250) NOT NULL PRIMARY KEY,
-    user_id VARCHAR(250) NOT NULL,
-    quiz_id VARCHAR(250) NOT NULL,
-    question_id VARCHAR(250) DEFAULT 0,
-    score_id VARCHAR(250) NOT NULL DEFAULT 0,
-    level INT NOT NULL DEFAULT 1,
-    total_questions INT NOT NULL,
-    completed BOOLEAN DEFAULT FALSE,
-    date_completed TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
-    FOREIGN KEY (
-        quiz_id,
-        level,
-        total_questions
-    ) REFERENCES quizzes (
-        quiz_id,
-        level,
-        total_questions
-    ) ON DELETE CASCADE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (question_id) REFERENCES questions (question_id) ON DELETE CASCADE,
-    FOREIGN KEY (score_id) REFERENCES scores (score_id) ON DELETE CASCADE
-);
-
 -- Create scores table
 CREATE TABLE IF NOT EXISTS scores (
     score_id VARCHAR(250) NOT NULL PRIMARY KEY,
@@ -82,17 +55,27 @@ CREATE TABLE IF NOT EXISTS scores (
     score INT NOT NULL DEFAULT 0,
     total_questions INT NOT NULL,
     quiz_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
-    FOREIGN KEY (quiz_date) REFERENCES progress (date_completed) ON DELETE CASCADE,
-    FOREIGN KEY (
-        quiz_id,
-        level,
-        total_questions
-    ) REFERENCES quizzes (
-        quiz_id,
-        level,
-        total_questions
-    ) ON DELETE CASCADE
+    FOREIGN KEY (quiz_id) REFERENCES quizzes (quiz_id) ON DELETE CASCADE
+);
+
+-- Create progress table
+CREATE TABLE IF NOT EXISTS progress (
+    progress_id VARCHAR(250) NOT NULL PRIMARY KEY,
+    user_id VARCHAR(250) NOT NULL,
+    quiz_id VARCHAR(250) NOT NULL,
+    question_id VARCHAR(250) DEFAULT 0,
+    score_id VARCHAR(250) DEFAULT NULL,
+    level INT NOT NULL DEFAULT 1,
+    total_questions INT NOT NULL,
+    completed BOOLEAN DEFAULT FALSE,
+    date_completed TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (quiz_id) REFERENCES quizzes (quiz_id) ON DELETE CASCADE,
+    FOREIGN KEY (question_id) REFERENCES questions (question_id) ON DELETE CASCADE,
+    FOREIGN KEY (score_id) REFERENCES scores (score_id) ON DELETE CASCADE
 );
