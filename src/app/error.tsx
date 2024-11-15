ad6f98168800e7a9ed24c9f0b127835ffc68bba7
Tina Huynh <tinamchuynh@gmail.com>
2024-11-15 12:33:11 -0800

    move the error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,32 +1,39 @@
 "use client";
 
-import React from "react";
-import { useEffect } from "react";
+import { secret } from "@/utils/secretUtils";
+import { useSearchParams } from "next/navigation";
 
-export default function Error( {
-  error,
-  reset,
-}: {
-  error: Error;
-  reset: () => void;
-} ) {
-  useEffect( () => {
-    // Log the error to an error reporting service
-    /* eslint-disable no-console */
-    console.error( error );
-  }, [error] );
+enum Error {
+    Configuration = "Configuration",
+}
 
-  return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
-  );
+const errorMap = {
+    [Error.Configuration]: (
+        <p className="text-gray-500 dark:text-gray-400 py-5">
+            There was a problem when trying to authenticate. Please contact us if this
+            error persists. Unique error code:{`${ secret }`}
+            <code className="rounded-sm bg-slate-100 p-1 text-xs">Configuration</code>
+        </p>
+    ),
+};
+
+export default function AuthErrorPage() {
+    const search = useSearchParams();
+    const error = search.get( "error" ) as Error;
+
+    return (
+        <div className="flex h-screen w-full flex-col items-center justify-center">
+            <a
+                href="#"
+                className="block max-w-sm rounded-lg border border-gray-200 bg-white p-6 text-center shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+            >
+                <h5 className="p-8 text-2xl font-extrabold text-center text-gray-900 dark:text-white">
+                    Something went wrong
+                </h5>
+                <div className="font-normal text-gray-700 dark:text-gray-400">
+                    {errorMap[error] || "Please contact us if this error persists."}
+                </div>
+            </a>
+        </div>
+    );
 }
